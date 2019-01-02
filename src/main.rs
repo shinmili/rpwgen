55017bb242aa7d2851606286e451be5100164931
Kuroki Keiichiro <shinmili@users.noreply.github.com>
2019-01-02 22:36:47 +0900

    Collect configurations into `Config` struct

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,49 @@
 extern crate clap;
 extern crate rand;
 
-use clap::App;
+use clap::{App, ArgMatches};
 use rand::prelude::*;
+use std::convert::From;
 
-fn generate(len: usize, choices: &[char]) -> String {
+struct Config {
+    choices: Vec<char>,
+    length: usize,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            choices: (b'0'..=b'9')
+                .chain(b'A'..=b'Z')
+                .chain(b'a'..=b'z')
+                .map(char::from)
+                .collect(),
+            length: 16,
+        }
+    }
+}
+
+impl<'a> From<ArgMatches<'a>> for Config {
+    fn from(matches: ArgMatches) -> Config {
+        let mut config = Config::default();
+        if let Some(choices) = matches.value_of("chars") {
+            if !choices.is_empty() {
+                config.choices = choices.chars().collect();
+            }
+        }
+        if let Some(length_str) = matches.value_of("length") {
+            if let Ok(length) = length_str.parse() {
+                config.length = length;
+            }
+        }
+        config
+    }
+}
+
+fn generate(config: &Config) -> String {
     let mut rng = thread_rng();
-    (0..len)
-        .map(|_| choices.choose(&mut rng).unwrap())
+    (0..config.length)
+        .map(|_| config.choices.choose(&mut rng).unwrap())
         .cloned()
         .collect()
 }
@@ -23,10 +59,5 @@ fn main() {
              [length] 'Password\'s length (in Unicode code points)'",
         )
         .get_matches();
-    let len = matches
-        .value_of("length")
-        .map(|s| s.parse().unwrap())
-        .unwrap();
-    let choices: Vec<char> = matches.value_of("chars").unwrap().chars().collect();
-    println!("{}", generate(len, &choices[..]));
+    println!("{}", generate(&matches.into()));
 }
