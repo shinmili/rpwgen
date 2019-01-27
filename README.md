599f18e458a263d1b14dd8ba6b2c938af8946866
Kuroki Keiichiro <shinmili@users.noreply.github.com>
2019-01-27 19:49:51 +0900

    Introduce range notation (like 0-9A-Za-z) for -c

--- README.md
+++ README.md
@@ -7,22 +7,22 @@ Generates password. Picked characters can be configured from command-line argume
 
 ## Usage
 
-oneliner:
+oneliner (with hiraganas):
 ```
-$ cargo run -- 16 --chars あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもらりるれろわをん
+$ cargo run -- 16 --chars ぁ-ん
     Finished dev [unoptimized + debuginfo] target(s) in 0.03s
-    Running `target/debug/rpwgen 16 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもらりるれろわをん'`
-めにしらめえあこわあしらねとらそ
+    Running `target/debug/rpwgen 16 --chars 'ぁ-ん'`
+ひえすゑゃぇえぅそぇどゎぎぅふい
 ```
 
-or build & run:
+or build & run (with alnums excluding confusing characters):
 ```
 $ cargo build --release
-$ ./target/release/rpwgen 16 --chars あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもらりるれろわをん
-ほろろるるにそなせれもほぬちれけ
+$ ./target/release/rpwgen 16 --chars A-HJ-NP-Za-km-z1-9
+ftEz9e8MfSVYiVXx
 ```
 
-The first argument (16 here) is the length of the generated password. The second argument (あいうえお... here) is the list of characters used in the generated password.
+Once built, `rpwgen --help` will show the help, but you don't need to take a look; The example above shows all the features implemented for now...
 
 ## License
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 extern crate clap;
 extern crate rand;
+#[macro_use]
+extern crate unic_char_range;
 
 use clap::ArgMatches;
 use rand::prelude::*;
@@ -13,14 +15,38 @@ pub struct Config {
 impl Default for Config {
     fn default() -> Self {
         Config {
-            choices: (b'0'..=b'9')
-                .chain(b'A'..=b'Z')
-                .chain(b'a'..=b'z')
-                .map(char::from)
-                .collect(),
+            choices: unfold_range_notation("0-9A-Za-z"),
             length: 16,
         }
     }
+}
+
+fn unfold_range_notation(s: &str) -> Vec<char> {
+    let mut choices = vec![];
+    let mut chars = s.chars().peekable();
+    while let Some(c) = chars.next() {
+        if let Some(&maybe_hyphen) = chars.peek() {
+            if maybe_hyphen == '-' {
+                if let Some(d) = chars.nth(1) {
+                    // /.-./
+                    for c in chars!(c..=d) {
+                        choices.push(c);
+                    }
+                } else {
+                    // /.-$/
+                    choices.push(c);
+                    choices.push('-');
+                }
+            } else {
+                // /.[^-]/
+                choices.push(c);
+            }
+        } else {
+            // /.$/
+            choices.push(c);
+        }
+    }
+    return choices;
 }
 
 impl<'a> From<ArgMatches<'a>> for Config {
@@ -28,7 +54,7 @@ impl<'a> From<ArgMatches<'a>> for Config {
         let mut config = Config::default();
         if let Some(choices) = matches.value_of("chars") {
             if !choices.is_empty() {
-                config.choices = choices.chars().collect();
+                config.choices = unfold_range_notation(choices)
             }
         }
         if let Some(length_str) = matches.value_of("length") {
@@ -46,4 +72,33 @@ pub fn generate(config: &Config) -> String {
         .map(|_| config.choices.choose(&mut rng).unwrap())
         .cloned()
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_unfold_range_notation() {
+        assert_eq!(
+            unfold_range_notation("abcABC012"),
+            "abcABC012".chars().collect::<Vec<char>>()
+        );
+        assert_eq!(
+            unfold_range_notation("a-g"),
+            "abcdefg".chars().collect::<Vec<char>>()
+        );
+        assert_eq!(
+            unfold_range_notation("a-gA-G"),
+            "abcdefgABCDEFG".chars().collect::<Vec<char>>()
+        );
+        assert_eq!(
+            unfold_range_notation("a-cde-g"),
+            "abcdefg".chars().collect::<Vec<char>>()
+        );
+        assert_eq!(
+            unfold_range_notation("e-gda-c"),
+            "efgdabc".chars().collect::<Vec<char>>()
+        );
+    }
 }
